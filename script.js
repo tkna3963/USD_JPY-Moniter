1818042432d3c27214721cde98fc755c1f0d6f45
tkna3963 <akiishigami853@gmail.com>
2024-08-01 18:16:35 +0900

    微調整

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ws.onmessage = (event) => {
 
     const timestampDate = new Date(timestamp);
     const formattedTimestamp = `取得時刻: ${timestampDate.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })}`;
-    if (timeList.length > 100) {
+    if (timeList.length > 10) {
         askList.shift(); 
         bidList.shift(); 
         timeList.shift();
